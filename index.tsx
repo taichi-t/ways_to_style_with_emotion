fdcb67c417e6e291189a1730bffc1a496a21a104
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-04-19 04:31:02 -0700

    :sparkles: create playground.

--- index.tsx
+++ index.tsx
@@ -3,7 +3,49 @@ import * as ReactDOM from 'react-dom';
 import styled from '@emotion/styled';
 
 const Main = () => {
-  return <h1>Hello world</h1>;
+  const [isHover, setIsHover] = React.useState(false);
+  return (
+    <>
+      <p className="red">外のテキスト</p>
+      <Container>
+        <Ul>
+          <p>ulのテキスト</p>
+          <Li>liのテキスト</Li>
+          <Li>liのテキスト</Li>
+        </Ul>
+        <Ul2>
+          <p>ulのテキスト</p>
+          <Li2>li2のテキスト</Li2>
+          <Li2>li2のテキスト</Li2>
+        </Ul2>
+        <button onClick={() => setIsHover(!isHover)}>hover</button>
+      </Container>
+    </>
+  );
 };
+
+const Container = styled('div')`
+  & .red {
+    color: red;
+  }
+`;
+
+const Ul = styled('ul')`
+  color: black;
+`;
+const Li = styled('ul')`
+  color: black;
+`;
+
+const Ul2 = styled('ul')`
+  color: black;
+
+`;
+const Li2 = styled('ul')`
+  color: black;
+  &:hover{
+    color:red
+  }
+`;
 
 ReactDOM.render(<Main />, document.getElementById('app'));
